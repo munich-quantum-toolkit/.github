7c49e13ea3a88043cf2ee55ed78bc3f565caf6fe
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2026-09-18 13:20:08 -0400

    🔖 Prepare release of `v1.4.0` (#209)

--- docs/conf.py
+++ docs/conf.py
@@ -22,8 +22,8 @@ if TYPE_CHECKING:
 
 project = "mqt"
 author = "Chair for Design Automation, TUM & Munich Quantum Software Company GmbH"
-version = "1.2"
-release = "1.3.0"
+version = "1.4"
+release = "1.4.0"
 language = "en"
 project_copyright = "2023 - 2026 Chair for Design Automation, TUM & 2025 - 2026 Munich Quantum Software Company GmbH"
 
